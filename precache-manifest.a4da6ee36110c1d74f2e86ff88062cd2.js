fb03d941bd57aaf83b50b33f2e6f68336fbe3e36
Mickey <vickeylee.w@gmail.com>
2020-06-07 00:07:14 +1000

    Updates

--- precache-manifest.a4da6ee36110c1d74f2e86ff88062cd2.js
+++ precache-manifest.a4da6ee36110c1d74f2e86ff88062cd2.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "686eb13ce6fc60b4e91d8f6d47c6de7c",
+    "revision": "bc4d176240242da1c70be200525a1a25",
     "url": "/FeedMe/index.html"
   },
   {
-    "revision": "328c472a2cb9307674d2",
+    "revision": "20448813c2abdfce7f9f",
     "url": "/FeedMe/static/css/main.65f9369d.chunk.css"
   },
   {
-    "revision": "3943929b856ed0d250e8",
-    "url": "/FeedMe/static/js/2.eb13c9d6.chunk.js"
+    "revision": "d8b5a336b55c9345dfc0",
+    "url": "/FeedMe/static/js/2.89ed921a.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/FeedMe/static/js/2.eb13c9d6.chunk.js.LICENSE.txt"
+    "url": "/FeedMe/static/js/2.89ed921a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "328c472a2cb9307674d2",
-    "url": "/FeedMe/static/js/main.665f6f77.chunk.js"
+    "revision": "20448813c2abdfce7f9f",
+    "url": "/FeedMe/static/js/main.32de7ea3.chunk.js"
   },
   {
     "revision": "009f46006ce4cb5cee04",
